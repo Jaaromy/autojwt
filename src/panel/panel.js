60c30336bc5e848c9af729fe9e3f1892c5cfc49a
Jaaromy Zierse <jaaromy.zierse@brivo.com>
2021-12-09 21:26:01 -0700

    Fix Chrome copy. Add response details.

--- src/panel/panel.js
+++ src/panel/panel.js
@@ -10,6 +10,11 @@ hljs.registerLanguage('json', json);
 let jwts = {};
 const JWT_REGEX = /(^[\w-]+\.[\w-]+\.[\w-]+$)/g;
 
+// Hack for Chrome font size issue
+if (navigator.userAgent.indexOf("Chrome") != -1 ) {
+    document.getElementsByTagName('body')[0].style.cssText = 'font-size: 110%';
+}
+
 function cleanUp(token) {
     return token
         .replace(/^bearer|^token|\s/ig, '');
@@ -26,9 +31,9 @@ function dateFormatter(data) {
     return moment(d).format('YYYY-MM-DD h:mm:ss a');
 }
 
-let gridOptions = {
+let mainGridOptions = {
     columnDefs: [
-        { headerName: 'JWT', field: 'jwt', maxWidth: 250, resizable: true },
+        { headerName: 'JWT', field: 'jwt', resizable: true },
         { headerName: 'Issuer', field: 'iss', resizable: true, sortable: true },
         { headerName: 'Issued (Local)', field: 'iat', resizable: true, sortable: true, valueFormatter: dateFormatter },
         { headerName: 'Expires (Local)', field: 'exp', resizable: true, sortable: true, valueFormatter: dateFormatter },
@@ -40,46 +45,84 @@ let gridOptions = {
     onRowSelected: onRowSelected
 };
 
+let responseGridOptions = {
+    columnDefs: [
+        { headerName: 'JWT', field: 'jwt', hide: true, suppressColumnsToolPanel: true},
+        { headerName: 'Url', field: 'url', resizable: true, sortable: true },
+        { headerName: 'Method', field: 'method', resizable: true, sortable: true },
+        { headerName: 'Status', field: 'status', resizable: true, sortable: true },
+        { headerName: 'Status Text', field: 'statusText', resizable: true, sortable: true }
+
+    ],
+    rowData: [
+    ],
+    rowSelection: 'single',
+    onSelectionChanged: onResponseRowSelectionChange
+};
+
+
 function onRowSelected(event) {
     if (event.node.isSelected()) {
+        let responseContents = document.getElementById('responseContents');
+        responseContents.innerHTML = '';
         let jwtContents = document.getElementById('jwtContents');
-        let urlList = document.getElementById('urlList');
-        urlList.replaceChildren();
         let decoded = jwts[event.node.data.jwt].decoded;
         let highlightedCode = hljs.highlight(JSON.stringify(decoded, undefined, 3), { language: 'json' }).value;
         jwtContents.innerHTML = highlightedCode;
 
+        responseGridOptions.api.setRowData([]);
+
         for (const urlTarget in jwts[event.node.data.jwt].targets) {
             if (Object.hasOwnProperty.call(jwts[event.node.data.jwt].targets, urlTarget)) {
-                //const target = jwts[event.node.data.jwt].targets[urlTarget];
-                
-                let li = document.createElement("li");
-                li.appendChild(document.createTextNode(urlTarget));
-                urlList.appendChild(li);
+                let response = jwts[event.node.data.jwt].targets[urlTarget];
+
+                responseGridOptions.api.applyTransaction({ add: [{ jwt: event.node.data.jwt, url: urlTarget, method: response.method, status: response.status, statusText: response.statusText }] });
+                autoSizeAll(false);
             }
         }
     }
 }
 
-function autoSizeAll(skipHeader) {
-    const allColumnIds = [];
-    gridOptions.columnApi.getAllColumns().forEach((column) => {
-        allColumnIds.push(column.colId);
-    });
+function onResponseRowSelectionChange(event) {
+    let responseContents = document.getElementById('responseContents');
+    responseContents.innerHTML = '';
 
-    gridOptions.columnApi.autoSizeColumns(allColumnIds, skipHeader);
+    let rows = event.api.getSelectedRows();
+
+    if (rows.length > 0) {
+        let response = jwts[rows[0].jwt].targets[rows[0].url];
+        let highlightedCode = hljs.highlight(JSON.stringify(response, undefined, 3), { language: 'json' }).value;
+        responseContents.innerHTML = highlightedCode;
+    }
 }
 
-let eGridDiv = document.querySelector('#myGrid');
-let grid = new Grid(eGridDiv, gridOptions);
-gridOptions.api.setDomLayout('autoHeight');
+function autoSizeAll(skipHeader) {
+    const mainColumnIds = [];
+    mainGridOptions.columnApi.getAllColumns().forEach((column) => {
+        mainColumnIds.push(column.colId);
+    });
+    mainGridOptions.columnApi.autoSizeColumns(mainColumnIds, skipHeader);
+    mainGridOptions.api.sizeColumnsToFit();
 
-// HACK: Remove when ready for real
-//autoSizeAll(false);
+    const responseColumnIds = [];
+    responseGridOptions.columnApi.getAllColumns().forEach((column) => {
+        responseColumnIds.push(column.colId);
+    });
+    responseGridOptions.columnApi.autoSizeColumns(responseColumnIds, skipHeader);
+
+}
+
+let mainGridDiv = document.querySelector('#mainGrid');
+let mainGrid = new Grid(mainGridDiv, mainGridOptions);
+mainGridOptions.api.setDomLayout('autoHeight');
+
+let responseGridDiv = document.querySelector('#responseGrid');
+let responseGrid = new Grid(responseGridDiv, responseGridOptions);
+responseGridOptions.api.setDomLayout('autoHeight');
 
 const jwtButton = document.getElementById('jwtio');
 jwtButton.addEventListener('click', () => {
-    let rows = gridOptions.api.getSelectedRows();
+    let rows = mainGridOptions.api.getSelectedRows();
 
     if (rows.length > 0) {
         runtime.sendMessage({
@@ -89,36 +132,66 @@ jwtButton.addEventListener('click', () => {
     }
 }, false);
 
+// Thanks to: https://stackoverflow.com/a/30810322
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
 const copyButton = document.getElementById('copy');
 copyButton.addEventListener('click', () => {
-    let rows = gridOptions.api.getSelectedRows();
+    let rows = mainGridOptions.api.getSelectedRows();
 
     if (rows.length > 0) {
         navigator.clipboard.writeText(rows[0].jwt).then(() => {
-            toConsole('Success');
-        }, (error) => {
-            toConsole('Failed Copy');
+        }, () => {
+            fallbackCopyTextToClipboard(rows[0].jwt);
+        });
+    }
+}, false);
+
+const copyPayloadButton = document.getElementById('copyPayload');
+copyPayloadButton.addEventListener('click', () => {
+    let rows = mainGridOptions.api.getSelectedRows();
+
+    if (rows.length > 0) {
+        let payload = JSON.stringify(jwts[rows[0].jwt].decoded, undefined, 3);
+
+        navigator.clipboard.writeText(payload).then(() => {
+        }, () => {
+            fallbackCopyTextToClipboard(payload);
         });
     }
 }, false);
 
 const clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', () => {
-    gridOptions.api.setRowData([]);
-    const contents = document.getElementById('jwtContents');
+    mainGridOptions.api.setRowData([]);
+    let contents = document.getElementById('jwtContents');
     contents.innerHTML = '';
     jwts = {};
-
-    let urlList = document.getElementById('urlList');
-    urlList.replaceChildren();
-
-}, false);
-
-const addRowButton = document.getElementById('add');
-addRowButton.addEventListener('click', () => {
-    addRow("https://supercoolsiteyeah.awesome.com?email=jaaromy.zierse@gmail.com", "Token eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5Ea3pNVVJFUXpneVJFUTNOelZGT1RBNVFqbEVSREE0TWtVelJqVTBOek15TnprM01VTTBRdyJ9.eyJodHRwczovL2JyaXZvLmNvbS9vbmFpcl91c2VyX2lkIjoxNjQ5NTQ0LCJodHRwczovL2JyaXZvLmNvbS9hY2NvdW50X2lkIjo1ODcyODMsImh0dHBzOi8vYnJpdm8uY29tL3VzZXJuYW1lIjoiSmFhcm9teUludC5za3ZweiIsImh0dHBzOi8vYnJpdm8uY29tL2VtYWlsIjoiamFhcm9teS56aWVyc2VAYnJpdm8uY29tIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi1pbnRhLmJyaXZvLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTFhOTMzYTE5MjhkOTAwNjgzNGQyNmQiLCJhdWQiOlsiaHR0cHM6Ly9hcGktaW50YS5icml2by5jb20iLCJodHRwczovL2JyaXZvLWludC5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjM5MDc5OTkzLCJleHAiOjE2MzkwODcxOTMsImF6cCI6Im1YVmdCTldMdlJFVm1qRWVXYmJHbzdVcGtCdEhaY3VqIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyIsInBlcm1pc3Npb25zIjpbXX0.i14sgoCNZF72U_wLwJLYslKrdA0E-MjlbKEE4EjN2C1483hwlL5FWVctxTP28Eygm6enQNQP6ELfK-Jl32ulwAEhbwgC-JWgp-Q_EA2KEwGOKmkUieh5Oo4jDYJwqtZWg9I-K52CBQbezRCq2oiyFrrFoIKz3p9ultO759yFfsJncnhNEaupMrA7CV8pSgxkrP9zi6nUv2F3AGkHC1u7OCxrQXhlHdmmtNq69eAP2y9-VG4Rn7TEwg3ttK0uZ6c87AdxmcRGYhZvq56MqjwmmOM0qbkHm6be1sa8VsyjHbGmmIvcFSGtFIMnCAGevnG0venREPgTqv0DeTdOeENPhw");
-    addRow("https://supercoolsiteyeah2.awesome.com?email=jaaromy.zierse@gmail.com", "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5Ea3pNVVJFUXpneVJFUTNOelZGT1RBNVFqbEVSREE0TWtVelJqVTBOek15TnprM01VTTBRdyJ9.eyJodHRwczovL2JyaXZvLmNvbS9vbmFpcl91c2VyX2lkIjoxNjQ5NTQ0LCJodHRwczovL2JyaXZvLmNvbS9hY2NvdW50X2lkIjo1ODcyODMsImh0dHBzOi8vYnJpdm8uY29tL3VzZXJuYW1lIjoiSmFhcm9teUludC5za3ZweiIsImh0dHBzOi8vYnJpdm8uY29tL2VtYWlsIjoiamFhcm9teS56aWVyc2VAYnJpdm8uY29tIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi1pbnRhLmJyaXZvLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTFhOTMzYTE5MjhkOTAwNjgzNGQyNmQiLCJhdWQiOlsiaHR0cHM6Ly9hcGktaW50YS5icml2by5jb20iLCJodHRwczovL2JyaXZvLWludC5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjM5MDc5OTkzLCJleHAiOjE2MzkwODcxOTMsImF6cCI6Im1YVmdCTldMdlJFVm1qRWVXYmJHbzdVcGtCdEhaY3VqIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyIsInBlcm1pc3Npb25zIjpbXX0.i14sgoCNZF72U_wLwJLYslKrdA0E-MjlbKEE4EjN2C1483hwlL5FWVctxTP28Eygm6enQNQP6ELfK-Jl32ulwAEhbwgC-JWgp-Q_EA2KEwGOKmkUieh5Oo4jDYJwqtZWg9I-K52CBQbezRCq2oiyFrrFoIKz3p9ultO759yFfsJncnhNEaupMrA7CV8pSgxkrP9zi6nUv2F3AGkHC1u7OCxrQXhlHdmmtNq69eAP2y9-VG4Rn7TEwg3ttK0uZ6c87AdxmcRGYhZvq56MqjwmmOM0qbkHm6be1sa8VsyjHbGmmIvcFSGtFIMnCAGevnG0venREPgTqv0DeTdOeENPhw");
-    addRow("https://supercoolsiteyeah4.awesome.com?email=jaaromy.zierse@gmail.com", "bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5Ea3pNVVJFUXpneVJFUTNOelZGT1RBNVFqbEVSREE0TWtVelJqVTBOek15TnprM01VTTBRdyJ9.eyJodHRwczovL2JyaXZvLmNvbS9vbmFpcl91c2VyX2lkIjoxNjQ5NTQ0LCJodHRwczovL2JyaXZvLmNvbS9hY2NvdW50X2lkIjo1ODcyODMsImh0dHBzOi8vYnJpdm8uY29tL3VzZXJuYW1lIjoiSmFhcm9teUludC5za3ZweiIsImh0dHBzOi8vYnJpdm8uY29tL2VtYWlsIjoiamFhcm9teS56aWVyc2VAYnJpdm8uY29tIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi1pbnRhLmJyaXZvLmNvbS8iLCJzdWIiOiJhdXRoMHw2MTFhOTMzYTE5MjhkOTAwNjgzNGQyNmQiLCJhdWQiOlsiaHR0cHM6Ly9hcGktaW50YS5icml2by5jb20iLCJodHRwczovL2JyaXZvLWludC5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNjM5MDc5OTkzLCJleHAiOjE2MzkwODcxOTMsImF6cCI6Im1YVmdCTldMdlJFVm1qRWVXYmJHbzdVcGtCdEhaY3VqIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCBvZmZsaW5lX2FjY2VzcyIsInBlcm1pc3Npb25zIjpbXX0.i14sgoCNZF72U_wLwJLYslKrdA0E-MjlbKEE4EjN2C1483hwlL5FWVctxTP28Eygm6enQNQP6ELfK-Jl32ulwAEhbwgC-JWgp-Q_EA2KEwGOKmkUieh5Oo4jDYJwqtZWg9I-K52CBQbezRCq2oiyFrrFoIKz3p9ultO759yFfsJncnhNEaupMrA7CV8pSgxkrP9zi6nUv2F3AGkHC1u7OCxrQXhlHdmmtNq69eAP2y9-VG4Rn7TEwg3ttK0uZ6c87AdxmcRGYhZvq56MqjwmmOM0qbkHm6be1sa8VsyjHbGmmIvcFSGtFIMnCAGevnG0venREPgTqv0DeTdOeENPhw");
+    responseGridOptions.api.setRowData([]);
+    let responseContents = document.getElementById('responseContents');
+    responseContents.innerHTML = '';
 }, false);
 
 function toConsole(item) {
@@ -127,7 +200,7 @@ function toConsole(item) {
         escape(JSON.stringify(item)) + '"))');
 }
 
-function addRow(url, token) {
+function addRow(request, response, token) {
     let tok = cleanUp(token);
 
     // Double parens around RegExp to get around this issue https://stackoverflow.com/a/68716278
@@ -139,13 +212,15 @@ function addRow(url, token) {
         const decoded = jwt_decode(tok);
         jwts[tok] = { decoded: decoded };
         jwts[tok].targets = {};
-        gridOptions.api.applyTransaction({ add: [{ jwt: tok, iss: decoded.iss, iat: decoded.iat, exp: decoded.exp }] });
+        mainGridOptions.api.applyTransaction({ add: [{ jwt: tok, iss: decoded.iss, iat: decoded.iat, exp: decoded.exp }] });
         autoSizeAll(false);
     }
 
-    if (jwts[tok] && !jwts[tok].targets[url]) {
-        jwts[tok].targets[url] = true;
-    }    
+    if (jwts[tok] && !jwts[tok].targets[request.url]) {
+        response.method = request.method;
+        jwts[tok].targets[request.url] = response;
+    }
+
 }
 
 devtools.network.onRequestFinished.addListener(
@@ -155,7 +230,7 @@ devtools.network.onRequestFinished.addListener(
                 const element = request.request.headers[header];
 
                 if (element && element.name && element.name.toLowerCase() === 'authorization') {
-                    addRow(request.request.url, element.value);
+                    addRow(request.request, request.response, element.value);
                 }
             }
         }
